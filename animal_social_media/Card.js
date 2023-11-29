8f650cb5fc7b7c90d2799ac7a2fcdc05d3276059
Mark Stanley <146277800+markstanl@users.noreply.github.com>
2023-11-29 17:16:21 -0600

    Update Card.js

--- animal_social_media/Card.js
+++ animal_social_media/Card.js
@@ -6,7 +6,7 @@ import Body from './Body';
 function Card(props){
   
   return(
-    <div>
+    <div class='card'>
       <Header profileImg={props.commentObject.profileImg} username={props.commentObject.username} />
       <Body comment={props.commentObject.comment}/>
     </div>
